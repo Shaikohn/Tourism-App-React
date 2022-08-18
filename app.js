e2309a5ce4f81460254f046ba3b230f996b7ab54
Juan <jgiacobbe32@gmail.com>
2022-08-17 22:31:31 -0300

    update

---
+++ app.js
@@ -0,0 +1,8 @@
+// const indexHtml = "index.html";
+// const formHtml = "form.html";
+// let actualUrl = window.location.href;
+
+// if(!(actualUrl.includes(indexHtml)) && !(actualUrl.includes(formHtml))) {
+//     window.location.replace("/", "/index.html");
+//     console.log("woork");
+// }
