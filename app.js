f074e371107f291563e15110bc9189e5dee2b904
juan <jgiacobbe32@gmail.com>
2022-08-26 21:31:39 -0300

    footer added

--- app.js
+++
@@ -1,8 +0,0 @@
-// const indexHtml = "index.html";
-// const formHtml = "form.html";
-// let actualUrl = window.location.href;
-
-// if(!(actualUrl.includes(indexHtml)) && !(actualUrl.includes(formHtml))) {
-//     window.location.replace("/", "/index.html");
-//     console.log("woork");
-// }
